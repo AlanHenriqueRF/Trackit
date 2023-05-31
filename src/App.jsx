e611cb7d1c34a2b3d5f792ded841654324ebc51e
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-05-31 08:17:15 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Em teste...
+       </div>
+    </>
+  )
+}
+
+export default App
