e83cf41d6df30f1baf95e0de2ee2d486845d7094
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-06-05 10:57:50 -0300

    Corrigido erro de data-tests iniciais;V02

--- src/Pages/SingUP/SingUpPage.jsx
+++ src/Pages/SingUP/SingUpPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Logo from './../../assets/logo.svg'
+import Logo from '../../assets/logo.svg'
 import { Link, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import axios from 'axios'
